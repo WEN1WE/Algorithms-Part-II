cccf562e27475892477a9c9ccb348a2ca589c277
WEN1WE <wwen8973@gmail.com>
2019-05-18 11:25:30 +0800

    Graph

---
+++ lectureCode/Graph/src/Graph.java
@@ -0,0 +1,56 @@
+import edu.princeton.cs.algs4.Bag;
+
+public class Graph {
+    private final int V;
+    private Bag<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<Integer>();
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adj(v)) degree++;
+        return degree;
+    }
+
+    /*
+    public static int maxDegree(Graph G) {
+        int max = 0;
+        for (int v = 0; v < G.V(); v++)
+            if (degree(G, v) > max)
+                max = degree(G, v);
+        return max;
+    }
+
+    public static double averageDegree(Graph G) {
+        return 2.0 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G) {
+        int count = 0;
+        for (int v = 0; v < G.V(); v++)
+            for (int w : G.adj(v))
+                if (v == w) count++;
+        return count / 2; // each edge counted twice
+    }
+     */
+
+
+}
