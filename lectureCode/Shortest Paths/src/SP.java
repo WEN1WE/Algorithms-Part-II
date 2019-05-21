6d9c90c04b7f9aa9d871dd2bf3a5189dc437ed73
WEN1WE <wwen8973@gmail.com>
2019-05-21 10:58:18 +0800

    SP

---
+++ lectureCode/Shortest Paths/src/SP.java
@@ -0,0 +1,26 @@
+import edu.princeton.cs.algs4.Stack;
+
+public class SP {
+    SP(EdgeWeightedDigraph G, int s) {
+
+    }
+
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
+            path.push(e);
+        return path;
+    }
+
+    private void relax(DirectedEdge e) {
+        int v = e.from(), w = e.to();
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+        }
+    }
+}
