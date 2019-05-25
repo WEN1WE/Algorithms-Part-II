0680d6cc8486a8d989d351b08935fbefe1aa8983
WEN1WE <wwen8973@gmail.com>
2019-05-25 11:12:10 +0800

    Finished

--- Programming Assignment/Baseball Elimination/src/BaseballElimination.java
+++ Programming Assignment/Baseball Elimination/src/BaseballElimination.java
@@ -7,130 +7,237 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.FlowEdge;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BaseballElimination {
-    private HashMap<String, int[]> teams2wlr;
-    private HashMap<Integer, String> i2team;
-    private int[][] g;
-    private int numberOfTeams;
+    private final HashMap<String, int[]> teams2wlr;
+    private final int[][] g;
+    private final int numberOfTeams;
+    private final int numberOfIJ;
+    private final int numberOfVertexes;
+    private String maxWinTeam;
 
+    /**
+     * Create a baseball division from given filename in format specified below
+     * @param filename filename
+     */
     public BaseballElimination(String filename) {
         In in = new In(filename);
         numberOfTeams = in.readInt();
+        numberOfIJ = (numberOfTeams * numberOfTeams - numberOfTeams) / 2;
+        numberOfVertexes = 2 + numberOfIJ + numberOfTeams;
         teams2wlr = new HashMap<>();
-        i2team = new HashMap<>();
         g = new int[numberOfTeams][numberOfTeams];
         for (int i = 0; i < numberOfTeams; i++) {
             String team = in.readString();
             int[] wlr = {in.readInt(), in.readInt(), in.readInt(), i};
             teams2wlr.put(team, wlr);
-            i2team.put(i, team);
             for (int j = 0; j < numberOfTeams; j++) {
                 g[i][j] = in.readInt();
             }
         }
+        maxWinTeam = getMaxWinTeam();
     }
 
+    /**
+     * Returns number of teams.
+     * @return number of teams
+     */
     public int numberOfTeams() {
         return numberOfTeams;
     }
 
+    /**
+     * Returns all teams.
+     * @return all teams
+     */
     public Iterable<String> teams() {
         return teams2wlr.keySet();
     }
 
+    /**
+     * Returns number of wins for given team.
+     * @param team team name
+     * @return number of wins for given team
+     */
     public int wins(String team) {
+        validate(team);
         return teams2wlr.get(team)[0];
     }
 
+    /**
+     * Returns number of losses for given team.
+     * @param team team name
+     * @return number of losses for given team
+     */
     public int losses(String team) {
+        validate(team);
         return teams2wlr.get(team)[1];
     }
 
+    /**
+     * Returns number of remaining games for given team.
+     * @param team team name
+     * @return number of remaining games for given team
+     */
     public int remaining(String team) {
+        validate(team);
         return teams2wlr.get(team)[2];
     }
 
+    /**
+     * Returns the index of the given team.
+     * @param team team name
+     * @return the index of the given team
+     */
     private int index(String team) {
+        validate(team);
         return teams2wlr.get(team)[3];
     }
 
-    public boolean isEliminated(String team) {
-
-
-        return true;
+    /**
+     * Returns number of remaining games between team1 and team2.
+     * @param team1 team1 name
+     * @param team2 team2 name
+     * @return number of remaining games between team1 and team2
+     */
+    public int against(String team1, String team2) {
+        validate(team1);
+        validate(team2);
+        return g[index(team1)][index(team2)];
     }
 
-    private FlowNetwork constructGraph(String team) {
-        int nTeamV = numberOfTeams - 1;
-        int nIJV = (nTeamV * nTeamV - nTeamV) / 2;
-        int nV = nTeamV + nIJV + 2;
-        int maxWin = wins(team) + remaining(team);
-        int indexOfteam = index(team);
-        int currentV = 1;
-        FlowNetwork network = new FlowNetwork(nV);
+    /**
+     * Is given team eliminated?
+     * @param team team name
+     * @return is given team eliminated?
+     */
+    public boolean isEliminated(String team) {
+        validate(team);
+        if (wins(maxWinTeam) > wins(team) + remaining(team)) {
+            return true;
+        }
 
-        int sign1 = 0;
+        FordFulkerson maxflow = getMaxflow(team);
+        int indexOfteam = index(team);
+        int currentVertex = 0;
         for (int i = 1; i < numberOfTeams; i++) {
-            if (i == indexOfteam) {
-                sign1 = -1;
-                continue;
-            }
-            int sign2 = 0;
-            for (int j = i + 1; j < numberOfTeams; j++) {
-                if (sign2 == indexOfteam) {
-                    sign2 = -1;
+            for (int j = i + 1; j <= numberOfTeams; j++) {
+                currentVertex++;
+                if (i == indexOfteam + 1 || j == indexOfteam + 1) {
                     continue;
                 }
-                network.addEdge(new FlowEdge(0, currentV, g[i + sign1][j + sign2]));
-                network.addEdge(new FlowEdge(currentV, nIJV + i + sign1, Double.POSITIVE_INFINITY));
-                network.addEdge(new FlowEdge(currentV, nIJV + j + sign2, Double.POSITIVE_INFINITY));
-                currentV += 1;
+                if (maxflow.inCut(currentVertex)) {
+                    return true;
+                }
             }
         }
-
-        int sign = 0;
-        for (int i = 1; i <= nTeamV ; i++) {
-            if (i2team.get(i).equals(team)) {
-                sign = -1;
-                continue;
-            }
-            network.addEdge(new FlowEdge(nIJV + i + sign, nV - 1, maxWin - wins(i2team.get(i))));
-        }
-        return network;
+        return false;
     }
 
-    public static void main(String[] args) {
+    /**
+     * Returns subset R of teams that eliminates given team; null if not eliminated.
+     * @param team team name
+     * @return subset R of teams that eliminates given team; null if not eliminated
+     */
+    public Iterable<String> certificateOfElimination(String team) {
+        validate(team);
+        ArrayList<String> subset = new ArrayList<>();
+        if (wins(maxWinTeam) > wins(team) + remaining(team)) {
+            subset.add(maxWinTeam);
+            return subset;
+        }
 
-        FlowNetwork f = new FlowNetwork(12);
-        f.addEdge(new FlowEdge(0, 1, 3));
-        f.addEdge(new FlowEdge(0, 2, 8));
-        f.addEdge(new FlowEdge(0, 3, 7));
-        f.addEdge(new FlowEdge(0, 4, 2));
-        f.addEdge(new FlowEdge(0, 5, 7));
-        f.addEdge(new FlowEdge(0, 6, 0));
-        f.addEdge(new FlowEdge(1, 7, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(1, 8, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(2, 7, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(2, 9, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(3, 7, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(3, 10, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(4, 8, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(4, 9, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(5, 8, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(5, 10, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(6, 9, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(6, 10, Double.POSITIVE_INFINITY));
-        f.addEdge(new FlowEdge(7, 11, 1));
-        f.addEdge(new FlowEdge(8, 11, 5));
-        f.addEdge(new FlowEdge(9, 11, 7));
-        f.addEdge(new FlowEdge(10, 11, 13));
-        System.out.println(f);
+        FordFulkerson maxflow = getMaxflow(team);
+        int indexOfteam = index(team);
+        int currentVertex = 0;
+        int sign = 0;
+        for (int i = 1; i < numberOfTeams; i++) {
+            for (int j = i + 1; j <= numberOfTeams; j++) {
+                currentVertex++;
+                if (i == indexOfteam + 1 || j == indexOfteam + 1) {
+                    continue;
+                }
+                if (maxflow.inCut(currentVertex)) {
+                    sign += 1;
+                    break;
+                }
+            }
+        }
 
-        BaseballElimination b = new BaseballElimination(args[0]);
-        FlowNetwork f1 = b.constructGraph("Detroit");
-        System.out.println(f1);
+        if (sign == 0) {
+            return null;
+        }
+
+        for (String currentTeam : teams()) {
+            if (currentTeam.equals(team)) {
+                continue;
+            }
+            if (maxflow.inCut(index(currentTeam) + numberOfIJ + 1)) {
+                subset.add(currentTeam);
+            }
+        }
+        return subset;
+    }
+
+    /**
+     * Construct maxflow that do not contain any vertex related to the given team.
+     * @param team team name
+     * @return maxflow of this problem
+     */
+    private FordFulkerson getMaxflow(String team) {
+        int maxWin = wins(team) + remaining(team);
+        int indexOfteam = index(team);
+        int currentVertex = 0;
+        FlowNetwork network = new FlowNetwork(numberOfVertexes);
+        FordFulkerson maxflow;
+
+        for (int i = 1; i < numberOfTeams; i++) {
+            for (int j = i + 1; j <= numberOfTeams; j++) {
+                currentVertex++;
+                if (i == indexOfteam + 1 || j == indexOfteam + 1) {
+                    continue;
+                }
+                network.addEdge(new FlowEdge(0, currentVertex, g[i - 1][j - 1]));
+                network.addEdge(new FlowEdge(currentVertex, numberOfIJ + i, Double.POSITIVE_INFINITY));
+                network.addEdge(new FlowEdge(currentVertex, numberOfIJ + j, Double.POSITIVE_INFINITY));
+            }
+        }
+
+        for (String currentTeam : teams()) {
+            if (currentTeam.equals(team)) {
+                continue;
+            }
+            network.addEdge(new FlowEdge(index(currentTeam) + numberOfIJ + 1, numberOfVertexes - 1, maxWin - wins(currentTeam)));
+        }
+
+        maxflow = new FordFulkerson(network, 0, numberOfVertexes - 1);
+        return maxflow;
+    }
+
+    /**
+     * Find the team with the max wins.
+     * @return team with the max wins.
+     */
+    private String getMaxWinTeam() {
+        for (String team : teams()) {
+            if (maxWinTeam == null) {
+                maxWinTeam = team;
+            } else if (wins(maxWinTeam) < wins(team)) {
+                maxWinTeam = team;
+            }
+        }
+        return maxWinTeam;
+    }
+
+    /**
+     * Throw exception if input argument is invalid team.
+     * @param team team name.
+     */
+    private void validate(String team) {
+        if (!teams2wlr.containsKey(team)) {
+            throw new java.lang.IllegalArgumentException();
+        }
     }
 }
